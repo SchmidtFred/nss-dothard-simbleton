00faab75f03e08ea8ddd6fabdb8c4d1265cdf5a7
Seracrooce <fschmidt@live.com>
2021-12-06 14:48:52 -0600

    Added in search functions for businesses and purchasing agents

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -1,12 +1,17 @@
 import { Business } from "./business.js";
-import { getBusinesses } from "./dataAccess.js";
+import { getBusinesses, getAgents } from "./dataAccess.js";
 import { businessListByState } from "./businessByState.js";
 import { businessListByIndustry } from "./businessByIndustry.js";
-import { purchasingAgents } from "./purchasingAgents.js";
+import { purchasingAgent } from "./purchasingAgents.js";
+
+const businessArray = getBusinesses();
+const agentArray = getAgents();
 
 export const businessList = () => {
-	const businessArray = getBusinesses();
-	let html = `<h1>Active Businesses</h1>
+	let html = `<input type="text" placeholder="Enter business name..." id="companySearch" />
+			<article class="foundCompanies">
+			</article>
+			<h1>Active Businesses</h1>
             <article class="businessList">`;
 	businessArray.forEach((businessObject) => {
 		html += Business(businessObject);
@@ -24,8 +29,31 @@ export const businessList = () => {
 			 </article>`;
 	//list purchasing agents
 	html += `<h1>Purchasing Agents</h1>
-			<article class="agentList">
-			${purchasingAgents()}
-			</article>`
+			<article class="agentList">`;
+	agentArray.forEach((agent) => {
+		html += purchasingAgent(agent);
+	});
+	html += `</article>`;
 	return html;
+};
+
+export const keyPressEvent = (event) => {
+	const companySearchResultArticle = document.querySelector(".foundCompanies");
+	if (event.charCode === 13) {
+		const foundBusiness = businessArray.find((bus) =>
+			bus.companyName.includes(event.target.value)
+		);
+		if (foundBusiness) {
+			companySearchResultArticle.innerHTML = Business(foundBusiness);
+		} else {
+			const foundAgent = agentArray.find((agent) =>
+				agent.fullName.includes(event.target.value)
+			);
+			if (foundAgent) {
+			companySearchResultArticle.innerHTML = purchasingAgent(foundAgent);
+			} else {
+				window.alert("Could not find search target");
+			}
+		}
+	}
 };
